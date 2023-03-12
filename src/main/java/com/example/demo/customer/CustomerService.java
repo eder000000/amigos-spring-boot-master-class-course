67c55a9fda6d4ce6af8f859d43e217a154079365
edera <edera@amdocs.com>
2023-03-11 23:27:51 -0600

    Getting the data from fakeDb

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -9,7 +10,7 @@ public class CustomerService {
 
     private final CustomerRepo customerRepo;
 
-    public CustomerService(CustomerRepo customerRepo){
+    public CustomerService(@Qualifier("fake") CustomerRepo customerRepo){
         this.customerRepo = customerRepo;
     }
      List<Customer> getCustomer(){
