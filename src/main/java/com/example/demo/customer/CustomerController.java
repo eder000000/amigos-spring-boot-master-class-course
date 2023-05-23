b4d98e042c5c5cd93c8ccd7a5caea0fcfdc37a4d
edera <edera@amdocs.com>
2023-05-23 00:48:03 -0600

    Add POST and DELETE mappings for Customer

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -19,4 +19,21 @@ public class CustomerController {
     List<Customer> getCustomer(){
         return customerService.getCustomer();
     }
+
+    @PostMapping
+    void createNewCustomer(@RequestBody Customer customer){
+        System.out.println("POST REQUEST");
+        System.out.println(customer);
+    }
+
+    @PutMapping
+    void updateCustomer(@RequestBody Customer customer){
+        System.out.println("UPDATE REQUEST...");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path="{customerId}")
+    void DeleteCustomer(@PathVariable("customerId")Long id){
+        System.out.println("DELETE REQUEST FOR CUSTOMER WITH ID:" + id);
+    }
 }
